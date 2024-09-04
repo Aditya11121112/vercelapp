4fa40d4b3b4d45e3fcaf76cfb6f867e33f159f04
Aditya <sd@taketwotechnologies.com>
2024-09-04 22:44:24 +0530

    added the cors thing

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ import { login } from "./controllers/login.controller.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 //apis for setup
 
