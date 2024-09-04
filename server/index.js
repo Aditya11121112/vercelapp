49509d5bec46cac41268597d45965a035a390235
Aditya <sd@taketwotechnologies.com>
2024-09-04 22:48:46 +0530

    frontend added

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "",
+    origin: "frontend-one-flax-26.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
