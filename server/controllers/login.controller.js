c66ab15491817dca8c83d78f73d2e6680e595584
Aditya <sd@taketwotechnologies.com>
2024-09-05 11:02:03 +0530

    jwt in login

--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -1,19 +1,30 @@
 import { userModel } from "../models/register.models.js";
+import jwt from "jsonwebtoken";
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   await userModel
     .findOne({ email })
-    .then((user) => {
+    .then(async (user) => {
       if (!user) {
         return res.json({
           message: "user not found please register",
           data: "",
         });
       }
+      const { email, name } = user;
 
       if (password == user.password) {
-        return res.json({ message: "user login succesfuuly", data: user });
+        //create the token on login
+        const access_token = await jwt.sign({ email, name }, "adi", {
+          expiresIn: "1h",
+        });
+        await res.cookie("access_token", access_token);
+        return res.json({
+          message: "user login succesfuuly",
+          data: user,
+          access_Token: access_token,
+        });
       }
 
       return res.json({ message: "Password incorrect", data: "" });
